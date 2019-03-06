bd8b35809e066329ee64825c7ac6a5760e4dab0a
Aditya Waruwu <aditya.william96@gmail.com>
2019-03-06 03:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var allMusicians = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var string =`${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
+  }
+  return allMusicians
+}
